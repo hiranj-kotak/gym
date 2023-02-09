018759a75475a379057757de684a1ce27cd06816
hiranj kotak <21it068@charusat.edu.in>
2023-02-09 15:24:17 +0530

    initial_commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gym',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("initila App"),
+      ),
+      body: const Center(
+        child: Text("hello"),
+      ),
+    );
+  }
+}
